7231467b44c6e03a5454f4f425b2eeb7602b29fe
velwot <velwott@gmail.com>
2026-08-09 23:04:56 +0530

    commit5

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+// @ts-ignore: allow importing CSS without type declarations
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
